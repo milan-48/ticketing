16fe5f1c9cafdadb60aec1a45df09bae8fdbf90d
milan-48 <mp890520@gmail.com>
2023-08-14 23:07:06 +0530

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -3,6 +3,7 @@ import { OrderStatus } from "./types/order-status";
 
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
+  version: number;
   data: {
     id: string;
     ticket: {
